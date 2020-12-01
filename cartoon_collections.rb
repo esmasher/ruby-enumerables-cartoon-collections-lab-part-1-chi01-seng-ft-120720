b8b0936d99f090c4eb00699461ef56e2b3c5ed18
Esma Sherwani <esmasherr@gmail.com>
2020-12-01 18:57:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def greet_characters(array)
-characters_array = ["Dopey","Hoggle", "Ludo", "Sir Didymus"]
+characters_array = ["Grumpy","Dopey","Hoggle", "Ludo", "Sir Didymus"]
 characters_array.each do |character|
    puts "Hello #{character}!"
   # Use `each` to enumerate over the provided array
